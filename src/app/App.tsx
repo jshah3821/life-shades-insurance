d2cf757686f9efa880b53a409f5046aec00b63ad
jay.s <jay.s@latitudetechnolabs.com>
2023-08-08 15:55:22 +0530

    removed extra packages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,27 +2,37 @@ import { lazy, Suspense, useState, useEffect } from "react";
 import Loader from "../components/layout/Loader";
 import NoInternet from "../Pages/NoInternet";
 import "../app.scss";
-import { gapi } from "gapi-script";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  const ReduxContainer = lazy(() => import("./ReduxContainer"));
+  const RouterContainer = lazy(() => import("./RouterContainer"));
   const [internet, setInternet] = useState(navigator.onLine);
-  const clientId =
-    "184488120753-obvs8d66gth0s2d9aoefqid2080t4m47.apps.googleusercontent.com";
 
   useEffect(() => {
     setInternet(navigator.onLine);
   }, [navigator.onLine]);
 
-  useEffect(() => {
-    gapi.load("client:auth2", () => {
-      gapi.auth2.init({ client_id: clientId });
-    });
-  }, []);
-
   return (
     <Suspense fallback={<Loader />}>
-      {internet ? <ReduxContainer /> : <NoInternet />}
+      {internet ? (
+        <>
+          <ToastContainer
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+          <RouterContainer />
+        </>
+      ) : (
+        <NoInternet />
+      )}
     </Suspense>
   );
 }
--- src/utils/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-// axios.defaults.withCredentials =true;
-const axiosInstance = axios.create({
-  //  withCredentials: true,
-  baseURL: process.env.REACT_APP_BASE_URL,
-});
-export default axiosInstance;
